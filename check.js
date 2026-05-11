b3feaf446718361238a348f8dbd6df89396c27de
JobPilot Dev <dev@jobpilot.local>
2026-05-11 18:22:42 +0200

    chore: clean up scratch files, design docs, and embedded git repo

--- check.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const cv = await prisma.cv.findUnique({ where: { id: 'cmo5q7cy40002hc0x0rt81s8z' } });
-  console.log('templateId:', cv.templateId);
-}
-
-main().finally(() => prisma.$disconnect());
